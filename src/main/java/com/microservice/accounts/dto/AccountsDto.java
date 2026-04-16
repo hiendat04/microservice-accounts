fe77c0dcbfdd492f9da68db455c3980acf414fc6
Dat Mai <hiendat04@gmail.com>
2026-04-16 18:36:51 -0500

    feat: add DTO classes for error handling and account/customer data

---
+++ src/main/java/com/microservice/accounts/dto/AccountsDto.java
@@ -0,0 +1,12 @@
+package com.microservice.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+    private Long customerId;
+
+    private String accountType;
+
+    private String branchAddress;
+}
